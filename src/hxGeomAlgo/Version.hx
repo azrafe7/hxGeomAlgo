86a1bc9408b982351fe3b77941eeef74904f82b9
azrafe7 <azrafe7@gmail.com>
2016-06-01 02:39:56 +0200

    Bump version (to 0.3.14)

--- src/hxGeomAlgo/Version.hx
+++ src/hxGeomAlgo/Version.hx
@@ -8,7 +8,7 @@ class Version
 {
 	inline public static var major:Int = 0;
 	inline public static var minor:Int = 3;
-	inline public static var patch:Int = 0;
+	inline public static var patch:Int = 14;
 	
 	static public function toString():String 
 	{
