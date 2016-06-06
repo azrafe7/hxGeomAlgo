bef229e3c652d5e846399d85a17954fc9588bc4b
azrafe7 <azrafe7@gmail.com>
2016-06-06 23:00:05 +0200

    [HertelMehlhorn] Initial impl.

--- src/hxGeomAlgo/HertelMehlhorn.hx
+++ src/hxGeomAlgo/HertelMehlhorn.hx
@@ -1,18 +1,10 @@
 /**
- * Bayazit polygon decomposition implementation.
- * NOTE: Should work only for SIMPLE polygons (not self-intersecting, without holes).
+ * Hertel-Mehlhorn convex polygonization from arbitrary triangulation.
  * 
  * Based on:
  * 
- * @see http://mnbayazit.com/406/bayazit							(C - by Mark Bayazit)
- * @see http://mnbayazit.com/406/credit
- * @see http://www.dyn4j.org/										(Java - by William Bittle)
- * 
- * Other credits should go to papers/work of: 
- * 
- * @see http://mnbayazit.com/406/files/PolygonDecomp-Keil.pdf		(Keil)
- * @see http://mnbayazit.com/406/files/OnTheTimeBound-Snoeyink.pdf	(Snoeyink & Keil)
- * @see http://www.cs.sfu.ca/~binay/								(Dr. Bhattacharya)
+ * @see https://github.com/ivanfratric/polypartition				(C - by Ivan Fratric)
+ * @see http://www.philvaz.com/compgeom								(by Phil Porvaznik)
  * 
  * @author azrafe7
  */
@@ -21,7 +13,6 @@ package hxGeomAlgo;
 
 
 import hxGeomAlgo.PolyTools;
-import hxPixels.Macro;
 
 
 using hxGeomAlgo.PolyTools;
@@ -31,24 +22,22 @@ using hxGeomAlgo.PolyTools;
 class HertelMehlhorn
 {
 	
-	static private var poly:Poly;		// cw version of simplePoly - used internally
-	static private var reflex:Map<Int, Bool>;		// vertices' indices reflexivity - used internally
+	static public var diagonals:Array<{from:HxPoint, to:HxPoint}>;
 	
-	static public var reflexVertices:Array<HxPoint> = new Array<HxPoint>();
-	static public var steinerPoints:Array<HxPoint> = new Array<HxPoint>();
-
-	static public var reversed:Bool;	// true if the _internal_ indices have been reversed
-
-	/** 
-	 * Decomposes `simplePoly` into a near-minimum number of convex polygons. 
+	/**
+	 * Merges triangles (defining a triangulated polygon) into a set of convex polygons.
+	 * 
+	 * @param	triangulation	An array of triangles defining the polygon.
+	 * @return	An array of convex polygons being a decomposition of the original polygon.
 	 */
 	static public function polygonize(triangulation:Array<Tri>):Array<Poly> {
 		var res:Array<Poly> = triangulation.concat([]);
+		diagonals = [];
 		
 		var poly:Poly, qoly:Poly, newPoly:Poly;
 		var p1:HxPoint, p2:HxPoint, p3:HxPoint;
 		var d1:HxPoint, d2:HxPoint;
-		var i11, i12, i21, i22, i13, i23;
+		var p:HxPoint, q:HxPoint;
 		var isDiagonal:Bool;
 		
 		var outerIt = 0;
@@ -76,7 +65,7 @@ class HertelMehlhorn
 				
 					qolyIt = 0;
 					while (qolyIt < qoly.length) {
-						var q = qoly.at(qolyIt);
+						q = qoly.at(qolyIt);
 						if (d2.x != q.x || d2.y != q.y) {
 							qolyIt++;
 							continue;
@@ -98,39 +87,59 @@ class HertelMehlhorn
 					continue;
 				}
 				
-				p2 = poly.at(polyIt);
-				p1 = poly.at(polyIt - 1);
-				p3 = qoly.at(polyIt + 1);
-				if (!PolyTools.isLeft(p1, p2, p3)) {
+				var d = ( { from:d1, to:d2 } );
+				diagonals.push(d);
+				
+				p = poly.at(polyIt + 2);
+				q = qoly.at(qolyIt - 1);
+				
+				var pTest = [
+					poly.at(polyIt),
+					q,
+					p,
+				];
+				
+				var qTest = [
+					qoly.at(qolyIt),
+					p,
+					q,
+				];
+				
+				if (PolyTools.isRightOrOn(pTest[0], pTest[1], pTest[2])) {
 					polyIt++;
 					continue;
 				}
 				
-				p2 = poly.at(polyIt + 1);
-				p3 = poly.at(polyIt + 2);
-				p1 = qoly.at(polyIt);
-				if (!PolyTools.isLeft(p1, p2, p3)) {
+				if (PolyTools.isRightOrOn(qTest[0], qTest[1], qTest[2])) {
 					polyIt++;
 					continue;
 				}
 				
+				// merge triangles/polys
 				newPoly = [];
-				var j = 0;
-				while (j != polyIt) {
+				var j = polyIt + 1;
+				while (poly.wrappedIdx(j) != polyIt) {
 					newPoly.push(poly.at(j));
 					j++;
 				}
-				j = 0;
-				while (j != qolyIt) {
+				j = qolyIt + 1;
+				while (qoly.wrappedIdx(j) != qolyIt) {
 					newPoly.push(qoly.at(j));
 					j++;
 				}
 				
-				res.splice(innerIt, 1);
-				res[outerIt] = newPoly;
-				poly = newPoly;
+				if (PolyTools.isConvex(newPoly)) {
+					res.splice(innerIt, 1);
+					res[outerIt] = newPoly;
+					poly = newPoly;
+					
+					polyIt = -1;	// restart
+				} else {
+					polyIt++;
+					continue;
+				}
 				
-				polyIt = 0;	// restart
+				polyIt++;
 			}
 			
 			outerIt++;
