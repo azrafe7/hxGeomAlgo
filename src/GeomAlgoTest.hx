b55ef417e7c64edcc7feb4056edbe49fdb57f8af
azrafe7 <azrafe7@gmail.com>
2020-09-24 01:41:11 +0200

    savePNG(): print b64-encoded BitmapData as 'data:image/png' chunk for all targets

--- src/GeomAlgoTest.hx
+++ src/GeomAlgoTest.hx
@@ -570,15 +570,15 @@ class GeomAlgoTest extends Sprite {
   static public function savePNG(bmd:BitmapData, fileName:String) {
     trace('savePNG("${fileName}")...');
     var ba:ByteArray = bmd.encode(bmd.rect, new PNGEncoderOptions());
+    var bytes:Bytes = Bytes.ofData(ba);
+    var b64Encoded = haxe.crypto.Base64.encode(bytes);
+    var len = b64Encoded.length;
+    trace('BitmapData base64-encoded ($len chars):');
+    trace('data:image/png;base64,$b64Encoded');
   #if (sys && (openfl && !nme))
     var bytes:Bytes = ba;
     sys.io.File.saveBytes(fileName, bytes);
     trace('BitmapData saved as "${fileName}".');
-  #else
-    var bytes:Bytes = Bytes.ofData(ba);
-    var b64Encoded = haxe.crypto.Base64.encode(bytes);
-    var len = b64Encoded.length;
-    trace('BitmapData base64-encoded ($len chars):\ndata:image/png;base64,$b64Encoded');
   #end
   }
 
